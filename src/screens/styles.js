26b3b8528b8db4e59ab98e38cbfa389c37ea595d
unknown <brunojsfla@gmail.com>
2021-10-29 08:13:28 -0300

    feat: implementação da legenda

--- src/screens/styles.js
+++ src/screens/styles.js
@@ -86,3 +86,22 @@ export const HomeContainer = styled.View`
   justify-content: center;
   align-items: center;
 `;
+export const Legend = styled.View`
+  width: 90%;
+  align-items: flex-start;
+  margin-top: 30px;
+`;
+export const LegendText = styled.Text`
+  color: #555;
+`;
+export const LegendItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 5px;
+`;
+export const LegendBox = styled.View`
+  width: 15px;
+  height: 15px;
+  background-color: #ccc;
+  margin-right: 5px;
+`;
